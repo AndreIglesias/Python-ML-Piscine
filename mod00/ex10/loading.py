e7e5d33839e42606b10f2e9831283d4c3c3beb8f
ciglesia <35022933+AndreIglesias@users.noreply.github.com>
2022-08-15 04:57:02 -0600

    load_percentage

--- mod00/ex10/loading.py
+++ mod00/ex10/loading.py
@@ -7,14 +7,14 @@ def ft_progress(lst):
     ln = len(lst)
     for i in range(ln):
         progress = (int((i / ln) * 20))
-        print('ETA: {:.02f}s [{: 3d}%] [{:20s}] {:}/{:}\
+        print('ETA: {:.02f}s [{:3d}%] [{:20s}] {:}/{:}\
  | elapsed time {:.02f}s'.format(
             (100 * (t2 - t) / (i + int(i == 0))) * ln, int(i / ln * 100),
             '='*progress + '>', i + 1, ln,
             100 * (time.process_time() - t)), end='\r')
         t2 = time.process_time()
         yield lst[i]
-    print('ETA: {:.02f}s [{: 3d}%] [{:20s}] {:}/{:}\
+    print('ETA: {:.02f}s [{:3d}%] [{:20s}] {:}/{:}\
  | elapsed time {:.02f}s'.format(
             100 * (time.process_time() - t), 100, '='*20, ln, ln,
             100 * (time.process_time() - t)), end='\r')
