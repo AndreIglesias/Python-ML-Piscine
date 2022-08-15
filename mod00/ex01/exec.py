673dbff8db04e321e84561ca5b7ccfb0859b0e7d
ciglesia <35022933+AndreIglesias@users.noreply.github.com>
2022-08-14 19:27:45 -0600

    empty_strings

--- mod00/ex01/exec.py
+++ mod00/ex01/exec.py
@@ -3,7 +3,8 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         for x in range(len(sys.argv) - 1, 0, -1):
-            print(sys.argv[x][::-1].swapcase() + ' ' * (x != 1), end='')
+            if sys.argv[x] != '':
+                print(sys.argv[x][::-1].swapcase() + ' ' * (x != 1), end='')
         print()
     else:
         print('Usage: python3 exec.py <Arg> [Arg] ...')
